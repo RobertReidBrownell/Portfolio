6f7cf3f7927bc2deffef1fc613ace5c7d4e82bd7
Robert Reid Brownell <reidbrownellhub@gmail.com>
2017-08-27 13:32:35 -0500

    First Commit for my Portfolio
    creating the structure and basic html for my Portfolio site.

---
+++ portfolio.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reidbrownell
+ * Date: 8/27/17
+ * Time: 1:10 PM
+ */
+?>
+
+<h2>Portfolio</h2>
+
+
+<h3>Golden Age Radio Restoration</h3>
+<p><b>Description:</b> My client wanted a site that brought the design and feeling of the golden age of radios to life. I used
+	the styling I did because I felt like it was a good representation of the design philosophies of that era.</p>
+<p><b>Skills:</b> I used HTML, CSS, and some SASS.</p>
+
+
+<h3>S.M.A.C.S.S.</h3>
+<p><b>Description:</b> My task was to research S.M.A.C.S.S. and build a single page website with the information I found and try
+	to use some of what I learned.</p>
+<p><b>Skills:</b> I used HTML, CSS, and JS.</p>
+
+
+<h3>Discography</h3>
+<p><b>Description:</b> My task was to research a band and then create a discography website of them and their albums.</p>
+<p><b>Skills:</b> I used HTML, CSS, JS, Bootstrap, and Photoshop.</p>
+
+
+<h3>PHP revamp</h3>
+<p><b>Description:</b> My task was to revamp a current site, implementing PHP to make the site more robust and full
+	featured while not changing any styles or design decisions of the original site.</p>
+<p><b>Skills:</b> I used HTML, CSS, JS, PHP, SQL, and phpMyAdmin .</p>
+
+<h3>OKVRC.ORG</h3>
+<p><b>Description:</b> My client wanted me to redo the old site they had and bring it into a CMS so they may add content
+	as they need to. This project will be a learning project as I will go back and refine more and more of it as I learn
+	more about building for the Wordpress CMS.</p>
+<p><b>Skills:</b> I used HTML, CSS, JS, PHP, Wordpress,  SQL, and phpMyAdmin</p>
+
